117b02f389903f4893c936bec897a71f7efb2c64
Allenye007 <15830003554@163.com>
2019-03-19 00:42:54 +0800

    index

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -17,24 +17,76 @@
         <div>场馆信息</div>
         <div>特色课程</div>
       </nav>
-      <div>
-
+      <div class="coach">
+        <div class="title">
+          <span>教练</span>
+          <span></span>
+          <span >更多</span >
+        </div>
+        <div class="person">
+          <div>allen</div>
+          <div>allen</div>
+          <div>allen</div>
+          <div>allen</div>
+        </div>
+      </div>
+      <div class="pic_wall">
+        <div class="title">
+          <span>照片墙</span>
+          <span></span>
+          <span >更多</span >
+        </div>
+        <div class="img_item">
+          <div class="img"><img src="https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640" alt=""></div>
+          <div class="img"><img src="https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640" alt=""></div>
+          <div class="img"><img src="https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640" alt=""></div>
+          <div class="img"><img src="https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640" alt=""></div>
+        </div>
       </div>
     </section>
   </div>
 </template>
 
 <style scoped>
-  .swiper {
-    margin-left: 50rpx;
-  }
+.swiper {
+  margin-left: 50rpx;
+}
 
-  nav {
-    display: flex;
-    justify-content: space-around;
-    height: 80rpx;
-    line-height: 80rpx;
-  }
+.title {
+  display: flex;
+  justify-content: space-between;
+  padding: 10rpx 50rpx;
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  height: 80rpx;
+  line-height: 80rpx;
+}
+
+.person {
+  display: flex;
+  justify-content: space-around;
+  height: 80rpx;
+  line-height: 80rpx;
+}
+
+.img_item {
+  display: flex;
+  justify-content: flex-start;
+  white-space: nowrap;
+}
+
+.img {
+  height: 200rpx;
+  width: 300rpx;
+}
+
+img image {
+  height: 200rpx;
+  width: 300rpx;
+}
 </style>
 
 <script>
@@ -42,11 +94,23 @@ export default {
   data() {
     return {
       banner: [
-        {image_url: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'},
-        {image_url: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'},
-        {image_url: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'},
-        {image_url: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'},
-      ],
+        {
+          image_url:
+            "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"
+        },
+        {
+          image_url:
+            "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"
+        },
+        {
+          image_url:
+            "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"
+        },
+        {
+          image_url:
+            "https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640"
+        }
+      ]
     };
   },
   methods: {},
